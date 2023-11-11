90da167f8b3cf54cd8b8e09e18de9cb4b7ef91cc
Aboltys754 <warwar754@mail.ru>
2023-11-12 00:14:07 +0500

    CRUD API

--- controllers/phoneBook.controllers.js
+++ controllers/phoneBook.controllers.js
@@ -1,24 +1,45 @@
+const db = require('../libs/db');
+
 module.exports.get = async (ctx) => {
+  const result = await db.query('select * from contacts where id=$1', [ctx.params.id])
+    .then((res) => res.rows[0]);
   ctx.status = 200;
-  ctx.body = 'get function';
+  ctx.body = result;
 };
 
 module.exports.getAll = async (ctx) => {
+  const result = await db.query('select * from contacts ')
+    .then((res) => res.rows);
   ctx.status = 200;
-  ctx.body = 'get function';
+  ctx.body = result;
 };
 
 module.exports.add = async (ctx) => {
+  const result = await db.query('INSERT INTO contacts (name, tel) VALUES ($1, $2) RETURNING *', [ctx.request.body.name, ctx.request.body.tel])
+    .then((res) => res.rows[0]);
   ctx.status = 201;
-  ctx.body = 'post function';
+  ctx.body = result;
 };
 
 module.exports.upd = async (ctx) => {
+  const result = await db.query(
+    'UPDATE contacts SET name=$1, tel=$2 WHERE id=$3 RETURNING *',
+    [
+      ctx.request.body.name,
+      ctx.request.body.tel,
+      ctx.params.id],
+  )
+    .then((res) => res.rows[0]);
   ctx.status = 200;
-  ctx.body = 'patch function';
+  ctx.body = result;
 };
 
 module.exports.del = async (ctx) => {
+  const result = await db.query(
+    'DELETE FROM contacts WHERE id=$1 RETURNING *',
+    [ctx.params.id],
+  )
+    .then((res) => res.rows[0]);
   ctx.status = 200;
-  ctx.body = 'delete function';
+  ctx.body = result;
 };
